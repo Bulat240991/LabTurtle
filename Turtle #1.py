d6590ac7de2808e194959f85d424b2802be24317
Булат <bulat240991@gmail.com>
2020-10-22 18:39:17 +0500

    Вложенные квадраты

--- Turtle #1.py
+++ Turtle #1.py
@@ -6,14 +6,14 @@ print("Задача:\nНарисовать черепашкой выбранну
       "1: Буква S\n"
       "2: Квадрат\n"
       "3: Круг\n"
-      "4: Пирамида\n"
+      "4: Вложенные квадраты\n"
       "5: Звезда\n"
       "6: Спираль\n"
       "7: Квадратная 'спираль'\n"
       "8: Правильные многоугольники")
 
 x = int(input())
-if x == 1                   # Буква S
+if x == 1:                   # Буква S
     turtle.shape('turtle')
     turtle.forward(50)
     turtle.left(90)
@@ -49,3 +49,25 @@ if x == 3:                  # Круг
         y += 1
     time.sleep(3)
 
+if x == 4:                  # Вложенные квадраты
+    print("Сколько нарисовать квадратов")
+    y = int(input())        # Указываем колличество квадратов, которое будет нарисованно
+    n = 20                  # Длинна грани первого квадрата в центре
+    while y > 0:
+        turtle.forward(n)
+        turtle.right(90)
+        turtle.forward(n)
+        turtle.right(90)
+        turtle.forward(n)
+        turtle.right(90)
+        turtle.forward(n)
+        turtle.penup()      # По окончанию прорисовки пергого квадрата, смещаемя на позицию для прорисовки следующего
+        turtle.left(90)
+        turtle.forward(10)
+        turtle.right(90)
+        turtle.forward(10)
+        turtle.right(90)
+        turtle.pendown()
+        n += 20             # Увеличиваем размер грани следущего квадрата
+        y -= 1
+        time.sleep(3)
